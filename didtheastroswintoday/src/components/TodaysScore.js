714e28b5bfe631734700b97ab13f089e3631639a
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-07-02 12:39:38 -0500

    Added API, Put JSON in local state

--- didtheastroswintoday/src/components/TodaysScore.js
+++ didtheastroswintoday/src/components/TodaysScore.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 import './TodaysScore.css';
 
 export class TodaysScore extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      games: []
+    }
+  }
+
+
+  WinCheck = () => {
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.sportradar.us/mlb-t6/games/2019/07/02/boxscore.json?api_key=82zanc4f93d4xd6x96awntpw`,{crossdomain:true})
+    .then(response => {
+      console.log(response)
+      this.setState({games: response.data.league.games})
+    })
+  }
 
   render(){
+
+
     return (
       <div>
-      <h2 className='TodaysScore'> Astros 15 - Yankees 8 </h2>
+      <button onClick={this.WinCheck}>Did they win?</button>
       </div>
     )
   }
