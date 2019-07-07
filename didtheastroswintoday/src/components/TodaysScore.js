6d14100652166630f834a24413fb5a5ce59a3577
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-07-07 18:14:10 -0500

    Changed API call to render all MLB games played on 7/7
    Continuing to work on logic to only use "todays" date.

--- didtheastroswintoday/src/components/TodaysScore.js
+++ didtheastroswintoday/src/components/TodaysScore.js
@@ -13,7 +13,7 @@ export class TodaysScore extends Component {
 
 
   WinCheck = () => {
-    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.sportradar.us/mlb-t6/games/2019/07/02/boxscore.json?api_key=82zanc4f93d4xd6x96awntpw`,{crossdomain:true})
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.sportradar.us/mlb-t6/games/2019/07/07/boxscore.json?api_key=82zanc4f93d4xd6x96awntpw`,{crossdomain:true})
     .then(response => {
       console.log(response)
       this.setState({games: response.data.league.games})
@@ -23,14 +23,13 @@ export class TodaysScore extends Component {
   render(){
 
     const gameinfo = this.state.games.map((info, index) => {
-      if (index === 8)
+      if (index === 1 || index === 2 || index === 3 || index === 4 || index === 5 || index === 6 || index === 7 || index === 8 || index === 9 || index === 10 || index === 11 || index === 12 || index === 13 || index === 14 || index === 15)
 
 
     {return(
       <div>
-      <ul>
-      <li className="ScoresList">
-      <p> Yes! </p>
+      <ul className="ScoresList">
+      <li>
       <p> {info.game.home.name} - {info.game.home.runs} </p>
       <p> {info.game.away.name} - {info.game.away.runs} </p>
       </li>
