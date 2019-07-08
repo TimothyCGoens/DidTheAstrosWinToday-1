145d7c4546ca9e5f03827267fd0f39c547b28921
Tim Goens <timcgoens@gmail.com>
2019-07-08 09:47:51 -0500

    changed the background

--- didtheastroswintoday/src/App.js
+++ didtheastroswintoday/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import TodaysScore from './components/TodaysScore'
 import './App.css';
 
 function App() {
   return (
     <div>
-    <h1 className='DTAWT'> Did the Astro's Win Today? </h1>
+    <h1 className='DTAWT'> Did the Astros Win Today? </h1>
     <TodaysScore />
     </div>
   );
